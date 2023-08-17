c5aeed73a70d5deac9c54ab2f57f86d48a76cdd3
lum-sydney <133835334+lum-sydney@users.noreply.github.com>
2023-08-17 16:57:06 -0400

    Add files via upload

---
+++ FlyingHelicopter.java
@@ -0,0 +1,70 @@
+/*Sydney Lum
+ * 8/17/2023
+ * 
+ * I want to make an animated ASCII flying helicopter that is 10 lines tall, 23 characters wide, that consists of two frames
+ */
+
+ public class FlyingHelicopter {
+
+    public static void main(String[] args) throws InterruptedException {    //Interrupted execution inserted in case there is an error
+        while(true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("            :---:----:----                       ");
+            System.out.println("              |            ");
+            System.out.println("          /--------                     ");
+            System.out.println(" ==+=-----      [ ] \\                ");
+            System.out.println("   |       \\         \\         ");
+            System.out.println("            \\_____ ___]               ");
+            System.out.println("               |    |        ");
+            System.out.println("          -------------/                 ");
+            System.out.println("                       ");
+            System.out.println("                       ");
+            
+            Thread.sleep(100, 0);  //100 milliseconds between frames so the helicopter isnt switching between frames too quickly
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("   ----:----:---:                       ");
+            System.out.println("              |            ");
+            System.out.println("   |      /--------                     ");
+            System.out.println(" ==+=-----      [ ] \\                ");
+            System.out.println("           \\         \\         ");
+            System.out.println("            \\_____ ___]               ");
+            System.out.println("               |    |        ");
+            System.out.println("          -------------/                 ");
+            System.out.println("                       ");
+            System.out.println("                       ");
+
+
+            Thread.sleep(100, 0);
+
+        }
+        
+        
+
+        
+    }
+
+
+ }
